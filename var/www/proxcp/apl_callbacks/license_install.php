7d49838522e2f05b82885828aad828edf7bf4899
Red Queen <red-queen@free-tools.club>
2023-11-24 16:10:07 +0100

    Data is NOT in headers. Its in the body!

--- var/www/proxcp/apl_callbacks/license_install.php
+++ var/www/proxcp/apl_callbacks/license_install.php
@@ -1,8 +1,9 @@
 <?php
-$LICENSE_CODE=$_SERVER['HTTP_LICENSE_CODE'];
-$APL_PRODUCT_ID=$_SERVER['HTTP_APL_PRODUCT_ID'];
-$CLIENT_EMAIL=$_SERVER['HTTP_CLIENT_EMAIL'];
-$ROOT_URL=$_SERVER['HTTP_ROOT_URL'];
+parse_str(urldecode($_SERVER['BODY']), $BODY);
+$LICENSE_CODE=$BODY['LICENSE_CODE'];
+$APL_PRODUCT_ID=$BODY['PRODUCT_ID'];
+$CLIENT_EMAIL=$BODY['CLIENT_EMAIL'];
+$ROOT_URL=$BODY['ROOT_URL'];
 $iparray = gethostbynamel("license.proxcp.free-tools.club");
 
 $server = hash("sha256", implode("", $iparray) . $APL_PRODUCT_ID . $LICENSE_CODE . $CLIENT_EMAIL . $ROOT_URL . gmdate("Y-m-d"));
@@ -12,5 +13,7 @@ header("notification_server_signature: $server");
 header("notification_case: notification_license_ok");
 header("notification_data: null");
 header("notification_text: Cracked by the Red Queen | discord.gg/free-tools");
-echo("Your calculated license_signature should be: $license_code\nThe Server signature is: $server")
+echo("Your calculated license_signature should be: $license_code\nThe Server signature is: $server\n\n\nDebug:\n");
+
+echo(var_dump($BODY));
 ?>
