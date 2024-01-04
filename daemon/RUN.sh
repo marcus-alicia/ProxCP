d480ee5ae814ce5345fedbf643767f393e6b9bb2
Red Queen <red-queen@free-tools.club>
2024-01-04 07:46:31 +0100

    Delete daemon/RUN.sh

--- daemon/RUN.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# Using this script to run proxcp-daemon will monitor the process for errors and keep it alive
-# forever start -a -l forever.log -e err.log -c /bin/bash RUN.sh
-exec ./proxcp-daemon
