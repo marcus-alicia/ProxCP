89fdcebd5f9a5da5a8cda0aa4ade84994dcc7530
Red Queen <red-queen@free-tools.club>
2024-02-01 00:50:39 +0100

    Update Credits

--- var/www/proxcp/apl_callbacks/license_verify.php
+++ var/www/proxcp/apl_callbacks/license_verify.php
@@ -12,7 +12,7 @@ $license_code = hash("sha256", gmdate("Y-m-d") . $ROOT_URL . $CLIENT_EMAIL . $LI
 header("notification_server_signature: $server");
 header("notification_case: notification_license_ok");
 header("notification_data: null");
-header("notification_text: Cracked by the Red Queen | discord.gg/free-tools");
+header("notification_text: Cracked by the Red Queen & Xxxosmo | discord.gg/free-tools");
 echo("Your calculated license_signature should be: $license_code\nThe Server signature is: $server\n\n\nDebug:\n");
 
 echo(var_dump($BODY));
